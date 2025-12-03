2030968b34c4321392b61ab6758fd3a1bfde9cd0
Floris van Drunen <floris.van.drunen@outlook.com>
2025-12-03 12:55:57 +0100

    display smg in footer

--- app/public/wp-content/plugins/FlorisPlugin/FlorisPlugin.php
+++ app/public/wp-content/plugins/FlorisPlugin/FlorisPlugin.php
@@ -3,3 +3,9 @@
 // Description: plugin voor saai school vak
 // Author: Floris
 // Version: 1.0
+
+add_action('wp_footer', 'florisplugin_display_message');
+
+function florisplugin_display_message() {
+    echo '<p style="text-align: center; font-size: 20px; color: blue;">Dit is een bericht van FlorisPlugin!</p>';
+}
